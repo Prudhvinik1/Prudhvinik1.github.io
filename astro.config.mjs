7c4081366e6aa6ec9933a5facab6b7f7ce5b402b
Prudhvi <snikku@umass.edu>
2024-07-25 20:59:38 -0400

    Adding changes to deploy.yml and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,51 @@
+// import { defineConfig } from 'astro/config'
+// import mdx from '@astrojs/mdx'
+// import tailwind from '@astrojs/tailwind'
+// import sitemap from '@astrojs/sitemap'
+// import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
+// import remarkUnwrapImages from 'remark-unwrap-images'
+// import rehypeExternalLinks from 'rehype-external-links'
+// import expressiveCode from 'astro-expressive-code'
+// import { expressiveCodeOptions } from './src/site.config'
+// import icon from 'astro-icon'
+
+// import vercel from '@astrojs/vercel/serverless'
+
+// // https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://prudhvinikku.me',
+// 	integrations: [
+// 		expressiveCode(expressiveCodeOptions),
+// 		tailwind({
+// 			applyBaseStyles: false
+// 		}),
+// 		sitemap(),
+// 		mdx(),
+// 		icon()
+// 	],
+// 	markdown: {
+// 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+// 		rehypePlugins: [
+// 			[
+// 				rehypeExternalLinks,
+// 				{
+// 					target: '_blank',
+// 					rel: ['nofollow, noopener, noreferrer']
+// 				}
+// 			]
+// 		],
+// 		remarkRehype: {
+// 			footnoteLabelProperties: {
+// 				className: ['']
+// 			}
+// 		}
+// 	},
+// 	prefetch: true,
+// 	output: 'server',
+// 	adapter: vercel({
+// 		webAnalytics: { enabled: true }
+// 	})
+// })
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
@@ -8,8 +56,6 @@ import rehypeExternalLinks from 'rehype-external-links'
 import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
-
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,8 +87,8 @@ export default defineConfig({
 		}
 	},
 	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
+	output: 'static',
+	build: {
+		outDir: 'dist'
+	}
 })
